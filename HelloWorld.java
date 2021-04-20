17a2ae09bde07583657a01698d2bd4217277fae2
Fabian F <h-f.fromme@posteo.de>
2021-04-20 14:20:54 +0200

    Add second Print-Line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hallo Fabian!");
+        System.out.println("Wie geht's dir denn heute so?");
     }
 }
