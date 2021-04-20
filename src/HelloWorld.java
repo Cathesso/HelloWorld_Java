1032d4be419d39d03f4f5c57874dddf180c6c409
Fabian F <h-f.fromme@posteo.de>
2021-04-20 21:05:37 +0200

    Reorder Java Development

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        String WelcomeMessage = "Well hello, Dolly!";
+        System.out.println("Hallo Fabian!");
+        System. out.println(WelcomeMessage);
+        System.out.println("Wie geht's dir denn heute so?");
+
+        int var1 = 481516;
+        int var2 = 2342108;
+        int result = var1 + var2;
+
+        System.out.println("Ergebnis der wissenschaftlichen Analyse: " + result);
+        System.out.println("Eins kleiner als zwei: " + (var1 < var2));
+        System.out.println("Eins kleiner als zwei: " + (var1 > var2));
+        System.out.println("Eins kleiner als zwei: " + (var1 == var2));
+
+    }
+}
